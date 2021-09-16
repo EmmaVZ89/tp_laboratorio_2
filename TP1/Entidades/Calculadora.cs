40801fa493f5e5468b66d3486e31ca0f97216543
Emmanuel Zelarayan <emmavz@live.com>
2021-09-15 21:11:44 -0300

    atributos, propiedad, metodos y contructores en clase operando

---
+++ TP1/Entidades/Calculadora.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Entidades
+{
+    public static class Calculadora
+    {
+        #region metodos
+        public static double Operar(Operando num1, Operando num2, char operador)
+        {
+            char operacion = Calculadora.ValidarOperador(operador);
+            double resultado = 0;
+
+            switch (operacion)
+            {
+                case '+':
+                    resultado = num1 + num2;
+                    break;
+                case '-':
+                    resultado = num1 - num2;
+                    break;
+                case '/':
+                    resultado = num1 / num2;
+                    break;
+                case '*':
+                    resultado = num1 * num2;
+                    break;
+            }
+            return resultado;
+        }
+
+        private static char ValidarOperador(char operador)
+        {
+            char retorno;
+
+            switch (operador)
+            {
+                case '+':
+                    retorno = '+';
+                    break;
+                case '-':
+                    retorno = '-';
+                    break;
+                case '/':
+                    retorno = '/';
+                    break;
+                case '*':
+                    retorno = '*';
+                    break;
+                default:
+                    retorno = '+';
+                    break;
+            }
+            return retorno;
+        }
+        #endregion
+    }
+}
---
+++ TP1/Entidades/Operando.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Operando
+    {
+        #region atributos
+        private double numero;
+        #endregion
+
+        #region propiedades
+        public string Numero
+        {
+            set
+            {
+                this.numero = this.ValidarOperando(value);
+            }
+        }
+        #endregion
+
+        #region metodos
+        public string BinarioDecimal (string binario)
+        {
+            string retorno = "Valor inválido";
+            int largoCadena;
+            int numeroDecimal;
+            int potencia;
+
+            if (this.EsBinario(binario))
+            {
+                largoCadena = binario.Length;
+                numeroDecimal = 0;
+                potencia = 0;
+                for (int i = largoCadena - 1; i >= 0; i--)
+                {
+                    if (binario[i] == '1')
+                    {
+                        numeroDecimal = (int)(numeroDecimal + Math.Pow(2, potencia));
+                    }
+                    potencia++;
+                }
+                retorno = numeroDecimal.ToString();
+            }
+
+            return retorno;
+        }
+
+        public string DecimalBinario (double numero)
+        {
+            string retorno = "";
+            numero = (int)numero;
+
+            if (numero > 0)
+            {
+                while (numero > 0)
+                {
+                    if (numero % 2 == 0)
+                    {
+                        retorno = "0" + retorno;
+                    }
+                    else
+                    {
+                        retorno = "1" + retorno;
+                    }
+                    numero = (int)(numero / 2);
+                }
+            }
+            else
+            {
+                if(numero == 0)
+                {
+                    retorno = "0";
+                }
+                else
+                {
+                    retorno = "Valor inválido";
+                }
+            }
+
+            return retorno;
+        }
+
+        public string DecimalBinario (string numero)
+        {
+            double numeroRetorno;
+            string retorno = "Valor Invalido";
+
+            if(Double.TryParse(numero, out numeroRetorno))
+            {
+                retorno = this.DecimalBinario(numeroRetorno);
+            }
+
+            return retorno;
+        }
+
+        private bool EsBinario (string binario)
+        {
+            bool retorno = true;
+
+            foreach (char letra in binario)
+            {
+                if(letra != '0' && letra != '1')
+                {
+                    retorno = false;
+                    break;
+                }
+            }
+
+            return retorno;
+        }
+
+        private double ValidarOperando (string strNumero)
+        {
+            double retorno;
+
+            if (Double.TryParse(strNumero, out retorno) == false)
+            {
+                retorno = 0;
+            }
+
+            return retorno;
+        }
+        #endregion
+
+        #region constructores
+        public Operando() : this(0)
+        {}
+
+        public Operando(double numero)
+        {
+            this.numero = numero;
+        }
+
+        public Operando(string strNumero)
+        {
+            this.Numero = strNumero;
+        }
+        #endregion
+
+        #region sobrecarga de operadores
+        public static double operator - (Operando n1, Operando n2)
+        {
+            return 0;
+        }
+
+        public static double operator * (Operando n1, Operando n2)
+        {
+            return 0;
+        }
+
+        public static double operator / (Operando n1, Operando n2)
+        {
+            return 0;
+        }
+
+        public static double operator + (Operando n1, Operando n2)
+        {
+            return 0;
+        }
+        #endregion
+    }
+}
